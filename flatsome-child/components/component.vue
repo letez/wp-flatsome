72146d8a770afb0233e19137113506fbde8f6274
Zan Lete <zanlete@Zans-MacBook-Pro.local>
2019-12-23 08:18:41 +0100

    wp-flatsome

---
+++ flatsome-child/components/component.vue
@@ -0,0 +1,18 @@
+<template>
+ 	<h3>{{ greeting }} Vue!</h3>
+</template>
+
+<script>
+module.exports = {
+  	mounted () { },
+	data: function() {
+		return {
+			greeting: 'Hello'
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
